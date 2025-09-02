d9a9c8293f9bb7dfc2e48a6e5f9d0e45b9140cfa
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-02 12:50:29 +0200

    feat: snack 2 completato

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+// snack 1
 import useSwitch from "./useSwitch";
+// snack 2
+import useDate from "./useDate";
 
 function App() {
+  // snack 1
   const [isOn, toggle] = useSwitch(false)
 
+  // snack 2
+  const currentDate = useDate();
+
   return (
-    <div>
-      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-      <button onClick={toggle}>Cambia Stato</button>
-    </div>
+    <>
+      {/* snack 1 */}
+      <div>
+        <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+        <button onClick={toggle}>Cambia Stato</button>
+      </div>
+
+      {/* snack 2 */}
+      <div>
+        <h1>Data e ora attuali:</h1>
+        <p>{currentDate.toLocaleString()}</p>
+      </div>
+    </>
+
   );
 }
 
---
+++ src/useDate.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+
+export default function useDate(){
+
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+       const currentDate = setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+        return () => clearInterval(currentDate)
+    }, [])
+
+    return date
+}
